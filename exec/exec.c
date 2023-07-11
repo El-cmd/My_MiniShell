50f1daded7a819a2d469ffd664ff4c4b658e443a
Valentin LOTH <vloth@paul-f4Ar5s11.clusters.42paris.fr>
2023-07-11 15:54:45 +0200

    leaks bad command

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: vloth <vloth@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:26:49 by vloth             #+#    #+#             */
-/*   Updated: 2023/07/10 15:26:02 by vloth            ###   ########.fr       */
+/*   Updated: 2023/07/11 14:47:56 by vloth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_execve(t_cmd *cmd, t_data *data)
 {
 	int		y;
 	char	*exec;
+	char	*tmp;
 	int		i;
 
 	y = 0;
@@ -28,14 +29,17 @@ void	ft_execve(t_cmd *cmd, t_data *data)
 			data->exit_return = execve(cmd->argv[0], cmd->argv, data->envp);
 			i++;
 		}
-		exec = ft_strjoin(ft_strjoin(data->path_exec[y], "/"), cmd->argv[0]);
+		tmp = ft_strjoin(data->path_exec[y], "/");
+		exec = ft_strjoin(tmp, cmd->argv[0]);
+		free(tmp);
 		data->exit_return = execve(exec, cmd->argv, data->envp);
 		free(exec);
 	}
 	ft_putstr_fd(cmd->argv[0], 2);
 	ft_putstr_fd(": command not found\n", 2);
-	data->exit_return = 127;
-	exit(data->exit_return);
+	free_everything(data);
+	i = 127;
+	exit(i);
 }
 
 void	exec(t_data *data)
