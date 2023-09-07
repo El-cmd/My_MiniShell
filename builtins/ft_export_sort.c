67a458be2af4f2d32559642fb75d5e6a958ba29b
Noe SPECIEL <nspeciel@made-f0Dr11s14.clusters.42paris.fr>
2023-09-07 07:55:52 +0200

    export mieux

--- builtins/ft_export_sort.c
+++ builtins/ft_export_sort.c
@@ -6,7 +6,7 @@
 /*   By: nspeciel <nspeciel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 20:51:34 by nspeciel          #+#    #+#             */
-/*   Updated: 2023/09/07 06:23:18 by nspeciel         ###   ########.fr       */
+/*   Updated: 2023/09/07 07:51:18 by nspeciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,33 +37,49 @@ void	free_env_names(char **env_names, int env_count)
 	free(env_names);
 }
 
-void	alloc_fill(t_env_som *env, char ***env_names, int *env_count)
-{
-	int		i;
-	t_env	*tmp;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	i = 0;
-	*env_count = 0;
-	tmp = env->begin;
-	while (tmp)
-	{
-		(*env_count)++;
-		tmp = tmp->next;
-	}
-	*env_names = (char **)malloc(*env_count * sizeof(char *));
-	if (!(*env_names))
-	{
-		printf("Erreur d'allocation mémoire.\n");
-		exit(1);
-	}
-	tmp = env->begin;
-	i = 0;
-	while (tmp)
-	{
-		(*env_names)[i] = strdup(tmp->name);
-		tmp = tmp->next;
-		i++;
-	}
+// ...
+
+void alloc_fill(t_env_som *env, char ***env_names, int *env_count)
+{
+    int i;
+    t_env *tmp;
+
+    i = 0;
+    *env_count = 0;
+    tmp = env->begin;
+    while (tmp)
+    {
+        (*env_count)++;
+        tmp = tmp->next;
+    }
+    *env_names = (char **)malloc(*env_count * sizeof(char *));
+    if (!(*env_names))
+    {
+        printf("Erreur d'allocation mémoire.\n");
+        exit(1);
+    }
+    tmp = env->begin;
+    i = 0;
+    while (tmp)
+    {
+        (*env_names)[i] = (char *)malloc((strlen(tmp->name) + 3) * sizeof(char));
+        if (!(*env_names)[i])
+        {
+            printf("Erreur d'allocation mémoire.\n");
+            exit(1);
+        }
+        char *equal_sign = strstr(tmp->name, "=");
+        if (equal_sign)
+            sprintf((*env_names)[i], "%.*s=\"%s\"", (int)(equal_sign - tmp->name), tmp->name, equal_sign + 1);
+        else
+            sprintf((*env_names)[i], "%s=\"\"", tmp->name);
+        tmp = tmp->next;
+        i++;
+    }
 }
 
 int	ft_export_whithout_arg(t_env_som *env, t_cmd *cmd, t_data *data)
