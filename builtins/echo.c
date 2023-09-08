ee5b14a9f25001bd84d69d0db99c24c1590a91ad
Noe SPECIEL <nspeciel@made-f0Cr11s6.clusters.42paris.fr>
2023-09-08 16:49:42 +0200

    echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,39 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vloth <vloth@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nspeciel <nspeciel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:26:14 by vloth             #+#    #+#             */
-/*   Updated: 2023/09/07 22:49:22 by vloth            ###   ########.fr       */
+/*   Updated: 2023/09/08 16:49:17 by nspeciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 //echo sans le \n
-void	ft_echo_n(t_cmd *cmd, int i)
-{
-	int	j;
-
-	j = 0;
-	while (cmd->argv[i])
-	{
-		while (cmd->argv[i][j])
-		{
-			if (cmd->argv[i][j] == '"' || cmd->argv[i][j] == '\'')
-				j++;
-			else
-				ft_putchar_fd(cmd->argv[i][j], 1);
-			j++;
-		}
-		if (cmd->argv[i + 1])
-			write(1, " ", 1);
-		j = 0;
-		i++;
-	}
-}
-
-//execute echo
 void	ft_echo_text(char *text)
 {
 	int	i;
@@ -49,66 +26,40 @@ void	ft_echo_text(char *text)
 	}
 }
 
-int	just_n(char *str)
+bool	is_valid_option(char *option)
 {
-	int	i;
+	int	j;
 
-	i = 2;
-	while (str[i])
+	if (option[0] == '-' && option[1] == 'n')
 	{
-		if (str[i] != 'n')
-			return (1);
-		i++;
+		j = 2;
+		while (option[j])
+		{
+			if (option[j] != 'n')
+				return (false);
+			j++;
+		}
+		return (true);
 	}
-	return (0);
-}
-
-int	pass_n(t_cmd *cmd)
-{
-	int	i;
-
-	i = 1;
-	while (cmd->argv[i] && !ft_strncmp(cmd->argv[i], "-n", 2))
-	{
-		if (just_n(cmd->argv[i]))
-			return (i);
-		i++;
-	}
-	return (i);
+	return (false);
 }
 
 void	ft_echo(t_cmd *cmd, t_data *data)
 {
 	int		i;
-	int		j;
-	bool	sup_newline;
-	bool	valid_opt;
+	bool	suppress_newline;
 
 	i = 1;
-	sup_newline = false;
-	if (cmd->argv[i] && (!ft_strcmp(cmd->argv[0], "echo")))
+	suppress_newline = false;
+	if (cmd->argv[i] && strcmp(cmd->argv[0], "echo") == 0)
 	{
 		while (cmd->argv[i])
 		{
-			if (!ft_strncmp(cmd->argv[i], "-n", 2))
+			if (is_valid_option(cmd->argv[i]))
 			{
-				j = 2;
-				valid_opt = true;
-				while (cmd->argv[i][j])
-				{
-					if (cmd->argv[i][j] != 'n')
-					{
-						valid_opt = false;
-						break ;
-					}
-					j++;
-				}
-				if (valid_opt)
-				{
-					sup_newline = true;
-					i++;
-					continue ;
-				}
+				suppress_newline = true;
+				i++;
+				continue ;
 			}
 			ft_echo_text(cmd->argv[i]);
 			if (cmd->argv[i + 1])
@@ -116,7 +67,7 @@ void	ft_echo(t_cmd *cmd, t_data *data)
 			i++;
 		}
 	}
-	if (!sup_newline)
-		ft_putchar_fd('\n', 1);
+	if (!suppress_newline)
+		write(1, "\n", 1);
 	data->exit_return = 0;
 }
